402ffeed449a23c316d1490b24ed619461dc0db7
Jaimie Nguyen <jaimietn@gmail.com>
2019-01-17 01:15:34 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
+var dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"]
+
 function dwarfRollCall(dwarves) {
+ var array =[]
+  for (var i=0;i<dwarves.length;i++){
+    array.push(`${i+1}. ${dwarves[i]} `)
+  }
+  return array.join("")
 }
+
+var planeteerCalls = ["earth", "wind", "fire", "water", "heart"]
 
 function summonCaptainPlanet(planeteerCalls){
+ return planeteerCalls.map(a=>a.toUpperCase()+"!")
+  return array.join(", ")
 }
 
-function longPlaneteerCalls(words) {
+var words = ["earth", "wind", "heart", "fire"]
+
+function longPlaneteerCalls(words){
+  for(var i=0;i<words.length;i++){
+    if (words[i].length>4){
+      return true
+    }
+  }
+  return false
 }
+
+var foods = ["cheddar", "gouda", "camembert"]
 
 function findTheCheese (foods) {
+  for(var i=0;i<foods.length;i++){
+  if (foods[i]=="cheddar"
+  ||foods[i]=="gouda"
+  ||foods[i]=="camembert"
+  ){
+    return foods[i]
+  }
+}
+return "no cheese!"
 }
