5ca82921ee00f3de66b3359d0766c9c649a9283b
Jaimie Nguyen <jaimietn@gmail.com>
2019-01-17 18:34:04 +0000

    Done.

--- index.js
+++ index.js
@@ -1,26 +1,22 @@
 var dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"]
-
 function dwarfRollCall(dwarves) {
- var array =[]
+  var array =[]
   for (var i=0;i<dwarves.length;i++){
     array.push(`${i+1}. ${dwarves[i]} `)
   }
   return array.join("")
-  
- dwarves.forEach((dwarf,i)=>array.push(`${i+1}. ${dwarves[i]} `))
- return array.join(" ")
 }
 
 var planeteerCalls = ["earth", "wind", "fire", "water", "heart"]
-
 function summonCaptainPlanet(planeteerCalls){
+  var array =[]
  return planeteerCalls.map(a=>a.toUpperCase()+"!")
   return array.join(", ")
 }
 
 var words = ["earth", "wind", "heart", "fire"]
-
 function longPlaneteerCalls(words){
+  var array =[]
   for(var i=0;i<words.length;i++){
     if (words[i].length>4){
       return true
@@ -30,8 +26,8 @@ function longPlaneteerCalls(words){
 }
 
 var foods = ["cheddar", "gouda", "camembert"]
-
 function findTheCheese (foods) {
+  var array =[]
   for(var i=0;i<foods.length;i++){
   if (foods[i]=="cheddar"
   ||foods[i]=="gouda"
